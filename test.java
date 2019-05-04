59616815e095590aa60674d6d719a9f60c0360d3
skobraf <skobraf@gmail.com>
2019-05-04 11:17:11 +0100

    fenetre avec JFrame

---
+++ test.java
@@ -0,0 +1,12 @@
+package projet;
+
+
+import javax.swing.JFrame;
+
+public class test {
+	public static void main(String[] args) {
+		JFrame fenetre = new JFrame();
+		fenetre.setSize(300,200);
+		fenetre.setVisible(true);
+	}
+}
