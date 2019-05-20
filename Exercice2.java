5c7e58a0aaec7a1c0e2d0a59bfc657d5b03fbc1b
skobraf <skobraf@gmail.com>
2019-05-20 18:49:00 +0100

    exercice 2 td 1

--- Exercice2.java
+++ Exercice2.java
@@ -1,38 +1,62 @@
-package ma.fsr.smi.tp3;
-
-import java.awt.Color;
-import java.awt.GridLayout;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
 
 public class Exercice2 {
+    private JFrame frame;
+    private JPanel contentPane;
+    private JPanel northPanel;
+    private JPanel labels, texts;
+    private JButton modifier;
+    private JLabel loginLabel, oldPassLabel, newPassLabel, confNewPassLabel;
+    private JTextField loginText;
+    private JPasswordField oldPassText, newPassText, confNewPassText;
 
-	@SuppressWarnings("serial")
-	static class Echeque extends JFrame {
-		Echeque() {
-			JPanel p = new JPanel(new GridLayout(8, 8));
-			
-			for (int i = 0; i < 8; i++) {
-				for (int j = 0; j < 8; j++) {
-					JButton b = new JButton();
-					b.setBackground((i + j) % 2 == 0 ? Color.white : Color.GRAY);
-					p.add(b);
-				}
-			}
-			
-			setTitle("Echeque");
-			setContentPane(p);
-			setVisible(true);
-			setSize(400, 400);
-			setLocationRelativeTo(null);
-			setDefaultCloseOperation(3);
-		}
-	}
+    Exercice2() {
+        frame = new JFrame();
+        contentPane = new JPanel(new BorderLayout());
+        northPanel = new JPanel(new BorderLayout());
+        labels = new JPanel(new GridLayout(0, 1));
+        texts = new JPanel(new GridLayout(0, 1));
+        modifier = new JButton("Modifier");
+        loginLabel = new JLabel("Login:");
+        oldPassLabel = new JLabel("Mot de passe:");
+        newPassLabel = new JLabel("Nouveau mot de passe:");
+        confNewPassLabel = new JLabel("Retapez le nouveau mot de passe:");
+        loginText = new JTextField(15);
+        oldPassText = new JPasswordField(15);
+        newPassText = new JPasswordField(15);
+        confNewPassText = new JPasswordField(15);
 
-	public static void main(String[] args) {
-		new Echeque();
-	}
+        labels.add(loginLabel);
+        labels.add(oldPassLabel);
+        labels.add(newPassLabel);
+        labels.add(confNewPassLabel);
 
+        texts.add(loginText);
+        texts.add(oldPassText);
+        texts.add(newPassText);
+        texts.add(confNewPassText);
+
+        northPanel.add(labels, "West");
+        northPanel.add(texts, "Center");
+
+        contentPane.add(northPanel, "North");
+        contentPane.add(modifier, "South");
+
+        modifier.addActionListener(e -> {
+            if (!newPassText.getText().trim().equals(confNewPassText.getText().trim())) {
+                JOptionPane.showMessageDialog(contentPane, "Tapez le meme mot de passe", "Erreur", JOptionPane.ERROR_MESSAGE);
+            }
+        });
+
+        frame.setContentPane(contentPane);
+        frame.setDefaultCloseOperation(3);
+        frame.setLocationRelativeTo(null);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Exercice2();
+    }
 }
